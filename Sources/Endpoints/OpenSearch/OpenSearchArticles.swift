0b46262e1b8a94e6013abf68af7b5e5755ff0bf3
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-08-29 16:22:56 +0900

    :+1: Add a search endpoint template with optional argument values

--- Sources/Endpoints/OpenSearch/OpenSearchArticles.swift
+++ Sources/Endpoints/OpenSearch/OpenSearchArticles.swift
@@ -16,6 +16,61 @@ public extension CiNiiKitArticles {
      https://support.nii.ac.jp/en/cia/api/a_opensearch
 
      - Parameters:
+     - keyword: Specifies keyword to be searched (necessary when items are not specified; items: title, author, authorid, issn, publisher, affilication, journal, volume, issue, page, references, year_to).
+         - count: Specifies the number of search results per page (optional)
+         - lang: Specifies the language the search results are displayed in (optional)
+         - start: Start number for the acquired search result list (optional)
+         - format: Specifies the format of the response (optional)
+         - title: earches by article name.
+         - author: Searches by author name.
+         - authorid: Searches by author id (NRID).
+         - issn: Searches by ISSN.
+         - publisher: Searches by publisher name. It is necessary to encode the URL.
+         - affiliation: Searches by author affiliation. It is necessary to encode the URL.
+         - journal: Searches by journal name. It is necessary to encode the URL.
+         - volume: Searches by volume.
+         - issue: Searches by issue.
+         - page: Searches by page.
+         - references: Searches by references. It is necessary to encode the URL.
+         - year_from: Searches by year of publication (From).
+         - year_to: Searches by year of publication (To).
+         - range: Specifies the search target option.
+         - sortorder: Specifies the sorting condition.
+         - success:
+         - failure:
+     */
+    func search(keyword q: String?,
+                count: Int? = nil,
+                lang: LanguageType? = nil,
+                start: Int? = nil,
+                format: Int? = nil,
+                title: String? = nil,
+                author: String? = nil,
+                authorid: String? = nil,
+                issn: String? = nil,
+                publisher: String? = nil,
+                affiliation: String? = nil,
+                journal: String? = nil,
+                volume: Int? = nil,
+                page: Int? = nil,
+                references: String? = nil,
+                year_from: Int? = nil,
+                year_to: Int? = nil,
+                range: Int? = nil,
+                sortorder: SortOrderType.Search? = nil,
+                success: CiNiiKit.SuccessHandler<[Any]>?,
+                failure: CiNiiKit.FailureHandler?) throws {
+        let oprionalParams: [Any?] = [q, title, author, authorid, issn, publisher, affiliation, journal, volume, page, references, year_from, year_to]
+        guard oprionalParams.filter({ $0 != nil }).isEmpty else {
+            throw QueryError.noSpecifiedKeywordError
+        }
+    }
+
+    /**
+
+     https://support.nii.ac.jp/en/cia/api/a_opensearch
+
+     - Parameters:
          - keyword: Specifies keyword to be searched.
          - count: Specifies the number of search results per page (optional)
          - lang: Specifies the language the search results are displayed in (optional)
---
+++ Sources/Error/ErrorHandring.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorHandring.swift
+//  CiNiiKit
+//
+//  Created by Yuto Mizutani on 2018/08/29.
+//
+
+import Foundation
+
+enum QueryError: Error {
+    case noSpecifiedKeywordError
+}
