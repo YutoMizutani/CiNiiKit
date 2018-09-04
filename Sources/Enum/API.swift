832f9fcc12620524039959aaabae6c3156aca1bb
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-05 03:05:12 +0900

    :memo: Add comment #19

--- Sources/Enum/API.swift
+++ Sources/Enum/API.swift
@@ -13,9 +13,13 @@ public struct API {
     public enum Articles {
         /// CiNii Articles OpenSearch API
         public enum OpenSearch {
+            /// Base URL
             private static let base = "http://ci.nii.ac.jp/opensearch"
+            /// CiNii Articles OpenSearch for Articles query
             static let search = "\(API.Articles.OpenSearch.base)/search"
+            /// CiNii Articles OpenSearch for Authors query
             static let author = "\(API.Articles.OpenSearch.base)/author"
+            /// CiNii Articles OpenSearch for Articles query
             static let fullText = "\(API.Articles.OpenSearch.base)/fulltext"
         }
     }
@@ -24,10 +28,15 @@ public struct API {
     public enum Books {
         /// CiNii Books OpenSearch API
         public enum OpenSearch {
+            /// Base URL
             private static let base = "http://ci.nii.ac.jp/books/opensearch"
+            /// CiNii Books OpenSearch query for Books & Journals
             static let search = "\(API.Books.OpenSearch.base)/search"
+            /// CiNii Books OpenSearch query for Authors
             static let author = "\(API.Books.OpenSearch.base)/author"
+            /// CiNii Books OpenSearch for Libraries query
             static let library = "\(API.Books.OpenSearch.base)/library"
+            /// CiNii Books OpenSearch for Holdings query
             static let holder = "\(API.Books.OpenSearch.base)/holder"
         }
     }
@@ -36,7 +45,9 @@ public struct API {
     public enum Dissertations {
         /// CiNii Dissertations OpenSearch API
         public enum OpenSearch {
+            /// Base URL
             private static let base = "http://ci.nii.ac.jp/d"
+            /// CiNii Dissertations OpenSearch query
             static let search = "\(API.Dissertations.OpenSearch.base)/search"
         }
     }
