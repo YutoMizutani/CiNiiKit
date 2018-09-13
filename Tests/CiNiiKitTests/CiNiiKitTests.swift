b0fffecd0473f20902281acb06b49ec59acb2c78
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-09-13 13:09:23 +0900

    :art: Add initializer when setup and remove example #37

--- Tests/CiNiiKitTests/CiNiiKitTests.swift
+++ Tests/CiNiiKitTests/CiNiiKitTests.swift
@@ -2,19 +2,19 @@ import XCTest
 @testable import CiNiiKit
 
 final class CiNiiKitTests: XCTestCase {
-    let helper = TestHelper.shared
-    let decoder = JSONDecoder()
+    var cinii: CiNiiKit!
+    var helper: TestHelper!
+    var decoder: JSONDecoder!
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let cinii = CiNiiKit.shared
-        XCTAssertEqual(cinii.text, "Hello, World!")
+    override func setUp() {
+        super.setUp()
+
+        self.cinii = CiNiiKit.shared
+        self.helper = TestHelper.shared
+        self.decoder = JSONDecoder()
     }
 
     static var allTests = [
-        ("testExample", testExample),
         ("testDecodeArticle", testDecodeArticle),
         ("testDecodeBooks", testDecodeBooks),
         ("testDecodeDissertations", testDecodeDissertations)
